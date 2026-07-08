8493961bd79dc7da79233e80a01102a0ca1e69ed
Harshita <monikadhaka721@gmail.com>
2026-07-08 21:35:39 +0530

    Add simple interest calculation functionality
    #This Simple Interest calculation check Whether Interest rate on principal and time.
    #Author : Harshita 
    #Date : 8/7/26

---
+++ Simple Interest Calculator.py
@@ -0,0 +1,6 @@
+p = flot(input ("Enter Principal Amount:"))
+r = flot(input ("Enter Rate of Interest:"))
+t = flot(input ("Enter Time (years):"))
+si = (p * r * t) / 100
+print ("simple interest =", si)
+# 
