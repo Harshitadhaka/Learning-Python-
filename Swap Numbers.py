e1cd673b747145d65a3d13d516661113bf57546e
Harshita <monikadhaka721@gmail.com>
2026-07-23 17:02:00 +0530

    Add program to swap two numbers
    #This program check Whether the given numbers and swap numbers and print it.
    #Author : Harshita 
    #Date : 23/7/26

---
+++ Swap Numbers.py
@@ -0,0 +1,16 @@
+# Program 29: Swap Two Numbers
+
+a = int(input("Enter first number :"))
+b = int(input("Enter Second number :"))
+
+print("Before swap")
+print("a =", a)
+print("b =", b)
+
+temp = a
+a = b
+b = temp
+
+print("After Swap")
+print("a =" , a)
+print("b =" , b)
