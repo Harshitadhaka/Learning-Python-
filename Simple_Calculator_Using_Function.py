dbd0aceaf5ab23b8b1d913940ef43e691d065036
Harshita <monikadhaka721@gmail.com>
2026-08-11 16:27:35 +0530

    Implement simple calculator with add and subtract functions
    # This program check the given numbers and print the Addition and Substraction of it.
    # Author : Harshita 
    # Date : 11/8/26

---
+++ Simple_Calculator_Using_Function.py
@@ -0,0 +1,11 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+a = int(input("Enter First Number: "))
+b = int(input("Enter Second Number: "))
+
+print("Addition =", add(a, b))
+print("Subtraction =", subtract(a, b))
